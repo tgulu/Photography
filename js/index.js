7fd1c39cb5585734512b4d6dfa97248f16c6f0e4
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2023-09-07 14:40:00 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const placesPhotography = document.getElementById("placesPhotography");
+placesPhotography.addEventListener("click", function () {
+  window.location.href = "/html/places.html";
+});
+
+const facesPhotography = document.getElementById("facesPhotography");
+facesPhotography.addEventListener("click", function () {
+  window.location.href = "/html/faces.html";
+});
---
+++ js/link.js
@@ -0,0 +1,4 @@
+const home = document.getElementById("home");
+home.addEventListener("click", function () {
+  window.location.href = "index.html";
+});
