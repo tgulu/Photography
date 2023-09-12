1abc33aad1cae5254b7ccb80061c1caeede82995
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2023-09-12 08:51:41 +0100

    fixed navbar for home page and refactored code

--- js/index.js
+++ js/index.js
@@ -7,3 +7,12 @@ const facesPhotography = document.getElementById("facesPhotography");
 facesPhotography.addEventListener("click", function () {
   window.location.href = "/html/faces.html";
 });
+
+
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
