615726f39af27a8e280423e9821b126afe884e0f
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2024-01-07 19:30:06 +0000

    fixed navbar in the main page and made it simpler, easier to use and more dynamic

--- js/index.js
+++ js/index.js
@@ -9,4 +9,28 @@ facesPhotography.addEventListener("click", function () {
 });
 
 
+// window.addEventListener('scroll', () => {
+//   const navbar = document.querySelector('header');
+//   navbar.classList.toggle('sticky', window.scrollY > 0);
+// });
 
+// const hamburger = document.querySelector(".hamburger")
+// const navMenu = document.querySelector(".nav-menu")
+
+
+// hamburger.addEventListener("click", () => {
+//   hamburger.classList.toggle("active");
+//   navMenu.classList.toggle('active');
+// })
+
+// document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
+//   hamburger.classList.remove("active");
+//   navMenu.classList.remove("active");
+// }))
+
+
+
+// window.addEventListener('scroll', () => {
+//   const navbar = document.querySelector('header');
+//   navbar.classList.toggle('sticky', window.scrollY > 0);
+// });
--- js/link.js
+++ js/link.js
@@ -1,5 +1,30 @@
-const home = document.getElementById("header");
-home.addEventListener("click", function () {
-  window.location.href = "/index.html";
+window.addEventListener('scroll', () => {
+  const navbar = document.querySelector('header');
+  navbar.classList.toggle('sticky', window.scrollY > 0);
 });
 
+const hamburger = document.querySelector(".hamburger")
+const navMenu = document.querySelector(".nav-menu")
+
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle('active');
+})
+
+document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
+
+
+
+const placesPhotography = document.getElementById("places-photography");
+placesPhotography.addEventListener("click", function () {
+  window.location.href = "/html/places.html";
+});
+
+const facesPhotography = document.getElementById("faces-photography");
+facesPhotography.addEventListener("click", function () {
+  window.location.href = "/html/faces.html";
+});
